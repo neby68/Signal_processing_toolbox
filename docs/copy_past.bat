a9dc67f8ad97fa0c8f048fbdaba087fdcc62148d
Nicolas EBY <n.eby@digitsole.com>
2023-11-02 08:46:38 +0100

    [major - update] update sphinx documentation with description and hyperlink

---
+++ docs/copy_past.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Copie des dossiers en cours...
+
+rem Spécifiez les chemins complets des dossiers source et de destination
+set "sourceFolder1=C:\Users\NicolasEBY\Documents\GitHub\Signal_processing_toolbox\docs\sphinx\_build"
+set "destinationFolder=C:\Users\NicolasEBY\Documents\GitHub\Signal_processing_toolbox\docs"
+
+rem Assurez-vous que le dossier de destination existe
+mkdir "%destinationFolder%" 2>nul
+
+rem Copiez les dossiers vers le dossier de destination
+xcopy /E /I "%sourceFolder1%" "%destinationFolder%\"
+
+echo Copie terminée.
+pause
--- docs/html/_sources/modules.rst.txt
+++ docs/html/_sources/modules.rst.txt
@@ -5,3 +5,12 @@ src
    :maxdepth: 4
 
    TimeSeriesDenoising
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Links:
+
+   Git Repository <https://github.com/neby68/Signal_processing_toolbox>
+   Electrical and neural signal processing examples' documentation <https://neby68.github.io/electrical_and_neural_signal_processing/>
+
