71a5bcbf16c9fc99bfc45b91c8d26f858c7c1f10
Nicolas EBY <n.eby@digitsole.com>
2023-11-02 21:13:34 +0100

    [minor - update] clean type_of_noise scprit

--- src/noise/type_of_noise.py
+++ src/noise/type_of_noise.py
@@ -1,26 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Création des fréquences
-hz = np.linspace(0, 1, 1000)
 
-# Création des spectres de bruit
-white_noise = np.ones_like(hz)
-pink_noise = 1 / np.sqrt(hz)
-brown_noise = 1 / hz
+def type_of_noise_plot():
+    """
+    Example of different noise types:
+        1. White noise:
+            Equal power across all frequencies.
+        2. Pink noise:
+            Power decreases at higher frequencies.
+        3. Brown noise:
+            Power decreases rapidly at higher frequencies.
 
-# Tracé du graphique
-plt.figure(figsize=(10, 6))
-plt.plot(hz, white_noise, label='White Noise')
-plt.plot(hz, pink_noise, label='Pink Noise')
-plt.plot(hz, brown_noise, label='Brown Noise')
+    .. image:: _static/images/noise/noise_type_description.png
 
-plt.xscale('log')
-plt.yscale('log')
+    
+    """
+    # Création des fréquences
+    hz = np.linspace(0, 1, 1000)
 
-plt.title('Spectral Density of Different Noises')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.legend()
-plt.grid(True)
-plt.show()
+    # Création des spectres de bruit
+    white_noise = np.ones_like(hz)
+    pink_noise = 1 / np.sqrt(hz)
+    brown_noise = 1 / hz
+
+    # Tracé du graphique
+    plt.figure(figsize=(10, 6))
+    plt.plot(hz, white_noise, label='White Noise')
+    plt.plot(hz, pink_noise, label='Pink Noise')
+    plt.plot(hz, brown_noise, label='Brown Noise')
+
+    plt.xscale('log')
+    plt.yscale('log')
+
+    plt.title('Spectral Density of Different Noises')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    type_of_noise_plot()
